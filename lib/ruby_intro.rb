33f18ae3337b9599b3b918d9eaf564a5f9aeb697
Pranav <pranavtaukari@gmail.com>
2022-09-10 16:39:13 -0500

    Completed Part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,19 +1,51 @@
 # frozen_string_literal: true
 
 # Part 1
-
 def sum(arr)
-  # YOUR CODE HERE
+  s = 0
+  if arr.length() == 0
+  	0
+  else
+  	for i in arr
+  		s = s + i
+  	end
+  	s
+  end
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  s = 0
+  if arr.length() == 0
+  	0
+  elsif arr.length() == 1
+  	arr[0]
+  else
+  	arr.sort.last(2).sum
+  end
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  
+  if arr.length() == 0 || arr.length() == 1 
+  	return false
+  else
+  	(0..arr.length-1).each do |i|
+  		x = i + 1
+  		#puts x
+  		n = number - arr[i]
+  		(x..arr.length-1).each do |j|
+  			if arr[j] == n
+  				#puts arr[i] + arr[j]
+  				return true
+  			end
+  		end
+  	end
+  end
+  return false
 end
 
+#sum_to_n? [-1,-2,3,4,5,-8],12
+=begin
 # Part 2
 
 def hello(name)
@@ -34,3 +66,4 @@ end
 class BookInStock
   # YOUR CODE HERE
 end
+=end
